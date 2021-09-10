9cfc733d423999e0eeb1cb0f2e2602f7ccbb55b8
naruhin <dimonkoko1@gmail.com>
2021-09-10 22:25:36 +0300

    annotation changes

--- src/main/java/com/naruhin/springbootexamplehillelhw5/SpringBootExampleHillelHw5Application.java
+++ src/main/java/com/naruhin/springbootexamplehillelhw5/SpringBootExampleHillelHw5Application.java
@@ -1,11 +1,12 @@
 package com.naruhin.springbootexamplehillelhw5;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Car API", version = "1.0", description = "Cars Information"))
 public class SpringBootExampleHillelHw5Application {
 
     public static void main(String[] args) {
